90e25e45aeaf5ae10d49843eae163addcbb13fdc
Brian Forbes <brianandrewf81@gmail.com>
2021-03-09 11:16:50 -0700

    Stub request and test page title and shortened URLs are visible

--- cypress/integration/URLShortener_spec.js
+++ cypress/integration/URLShortener_spec.js
@@ -1,16 +1,40 @@
 // Write Cypress tests for the following user flows (don't forget to stub your network requests):
 //
-// When a user visits the page, they can view the page title and the existing shortened URLs
 // When a user visits the page, they can view the Form with the proper inputs
 // When a user fills out the form, the information is reflected in the input fields
 
-describe('URL Shortener', () => {
-const baseUrl = 'http://localhost:3000';
+    describe('URL Shortener', () => {
+    const baseUrl = 'http://localhost:3000';
 
-it('should be able to fetch a random curated taco', () => {
-    cy
-    .intercept('GET', 'http://localhost:3001/api/v1/urls', {fixture: 'curatedData.json'})
-    cy
-    .visit(baseUrl);
-})
-})
+    beforeEach(() => {
+        cy
+        .intercept('GET', 'http://localhost:3001/api/v1/urls', {fixture: 'getDummyData.json'})
+        cy
+        .visit(baseUrl);
+          });
+
+    it('user should be able to view page title', () => {
+        cy
+        .get('h1').contains('URL Shortener')
+        cy
+        .get('a:first').should('have.attr', 'href', 'http://localhost:3001/useshorturl/1')
+        cy
+        .get('a:last').should('have.attr', 'href', 'http://localhost:3001/useshorturl/2')
+    })
+
+    it('user should be able to view input fields for title and url to shorten', () => {
+
+    })
+    //
+    // it('user should be able to view page title', () => {
+    //
+    // })
+    //
+    // it('user should be able to view page title', () => {
+    //
+    // })
+    //
+    // it('user should be able to view page title', () => {
+    //
+  // })
+  })
