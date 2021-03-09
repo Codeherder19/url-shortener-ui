82371be6e4c5e0d4d9edb2943203fc166ff2a69d
Brian Forbes <brianandrewf81@gmail.com>
2021-03-09 12:06:28 -0700

    Test that a user can see the shortened URL on the page after submitting a title and url in the form

--- cypress/integration/testPostRequest_spec.js
+++ cypress/integration/testPostRequest_spec.js
@@ -2,16 +2,29 @@ describe('URL Shortener', () => {
   const baseUrl = 'http://localhost:3000';
 
   beforeEach(() => {
+    cy
+    .fixture('postDummyData.json')
+    .then(post => {
       cy
-      .fixture('postDummyData.json')
-      .then(post => {
-        cy
-        .intercept('POST', 'http://localhost:3001/api/v1/urls', {
-          statuseCode: 201,
-          body: post
-        })
+      .intercept('POST', 'http://localhost:3001/api/v1/urls', {
+        statusCode: 201,
+        body: post
       })
-      cy
-      .visit(baseUrl)
+    })
+    cy
+    .visit(baseUrl)
   });
+
+  it('user should be able to see a new shortened URL rendered after form submission', () => {
+    cy
+    .get('input:first').type('Colorado Unemployment')
+    cy
+    .get('input:last').type('https://cdle.colorado.gov/unemployment')
+    cy
+    .get('button').click()
+    cy
+    .get('a:last').should('have.attr', 'href', 'http://localhost:3001/useshorturl/4')
+  })
+
+
 })
